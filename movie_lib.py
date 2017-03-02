ae1baf0a8756c83f9aba4a19867fe32e4e82cb61
Rajan Khanna <rajankhanna@Rajan-Khannas-MacBook-Pro.local>
2017-03-02 15:55:16 -0500

    First draft, finished inits for Movie and User classes and opening the two files to create three dictionaries

---
+++ movie_lib.py
@@ -0,0 +1,44 @@
+import csv
+from statistics import mean
+
+
+class Movie:
+    def __init__(self, item_id):
+        self.item_id = item_id
+        self.avg_rtg = 0
+
+    def avg_rtg(self, rtg_by_movie_id):
+        return mean(rtg_by_movie_id(self.item_id))
+
+
+class User:
+    def __init__(self, user_id):
+        self.user_id = user_id
+
+
+
+
+rtg_by_movie_id = {}
+rtg_by_user_id = {}
+
+with open('u.data') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        row = row[0].split('/t')
+        row[2] = int(row[2])
+        if row[1] in rtg_by_movie_id:
+            rtg_by_movie_id[row[1]].append(row[2])
+        else:
+            rtg_by_movie_id[row[1]] = [row[2]]
+        if row[0] in rtg_by_user_id:
+            rtg_by_user_id[row[0]].append(row[2])
+        else:
+            rtg_by_user_id[row[0]] = [row[2]]
+
+movie_names = {}
+
+with open('u.item') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        row = row[0].split('/t')
+        movie_names[row[0]] = [row[1]]
